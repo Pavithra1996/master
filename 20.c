d216db84f92e9e00485fae7ba73039959b680ea6
Pavithra1996 <pavisindhu02@gmail.com>
2016-07-06 12:43:03 +0530

    Create 20.c

---
+++ 20.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int z,n,i;
+clrscr();
+printf("Enter the number");
+scanf(%d",n);
+for(i=0;i<=9;i++)
+{
+z=n*i;
+printf("%d",z);
+}
+getch();
+}
