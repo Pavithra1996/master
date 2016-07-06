df0f5cfed122f33422f51c7c740d696ea3277644
Pavithra1996 <pavisindhu02@gmail.com>
2016-07-06 12:21:51 +0530

    Update 5.c

--- 5.c
+++ 5.c
@@ -8,7 +8,7 @@ printf("Enter three numbers");
 scanf("%d %d %d",&a,&b,&c);
 if((a>b)&&(a>c))
 printf("a is greater");
-elseif((b>a)&&(b>c))
+else if((b>a)&&(b>c))
 printf("b is greater");
 else
 printf("c is greater");
