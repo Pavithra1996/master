6f4adc2975b4a11a6e77ec34ccd70c5f99cef640
Pavithra1996 <pavisindhu02@gmail.com>
2016-07-06 14:22:57 +0530

    Update fibo.c

--- fibo.c
+++ fibo.c
@@ -1,13 +1,39 @@
-#include<stdio.h>
-#include<conio.h>
-int main()
+package com.guvi.codekata;
+
+import java.util.Scanner;
+
+/*
+ * 
+ * 
+ */
+public class Program21 
 {
-int no;
-printf("\nEnter a number");
-scanf("%d",&no);
-for(int i=1;i<=15;i++)
-{
-	printf("\n%d * %d = %d",no,i,no*i);
-}
-return 0;
+	public void fibonacci(int num)
+	{
+		//System.out.println(num);
+		int[] fib=new int[num];
+		fib[0]=0;
+		System.out.println(fib[0]);
+		
+		fib[1]=1;
+		System.out.println(fib[1]);
+		
+		for(int i=2;i<num;i++)
+		{
+			fib[i]=fib[i-1]+fib[i-2];
+			System.out.println(fib[i]);
+				
+		}
+			}
+	
+	public static void main(String[] args)
+	{
+		Scanner sc=new Scanner(System.in);
+		Program21 pro=new Program21();
+		pro.fibonacci(5);
+		
+		
+		
+	}
+
 }
